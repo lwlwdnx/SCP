535f4bb8f29b91634e07c641cbd353e95f08a517
Li Wanlong <lwlwdnx@gmail.com>
2019-09-22 11:35:07 +0900

    death

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary") 
+        {
+            return;
+        }
+
+        if (other.tag == "main camera")
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		
+	}
+}
