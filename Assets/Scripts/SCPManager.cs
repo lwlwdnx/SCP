b73c701d9b2c1f612ef47703b43717d711360425
Li Wanlong <lwlwdnx@gmail.com>
2019-09-22 13:41:52 +0900

    trigger

--- Assets/Scripts/SCPManager.cs
+++ Assets/Scripts/SCPManager.cs
@@ -7,10 +7,19 @@ public class SCPManager : MonoBehaviour
     [SerializeField] GameObject[] enemys;
     List<GameObject> enemy_list = new List<GameObject>();
 
+    private LightUp lightUpScript;
+
     // Use this for initialization
     void Start()
 	{
-		enemy_list.Add(GameObject.Instantiate(enemys[0], transform.position, Quaternion.identity));
+        GameObject go = GameObject.FindWithTag("LightUp");
+        if (go != null) 
+        {
+            lightUpScript = go.GetComponent<LightUp>();
+            //Debug.Log("000");
+        }
+
+        enemy_list.Add(GameObject.Instantiate(enemys[0], transform.position, Quaternion.identity));
         enemy_list.Add(GameObject.Instantiate(enemys[0], transform.position, Quaternion.identity));
 
         foreach (GameObject obj in enemy_list)
@@ -18,11 +27,18 @@ public class SCPManager : MonoBehaviour
             obj.GetComponent<Enemy>().position = new Vector3(10, 0, 0);
             obj.GetComponent<Enemy>().enemy_state = Enemy.EnemyState.PATROL;
         }
-        enemy_list[1].GetComponent<Enemy>().enemy_state = Enemy.EnemyState.CHASE;
+        
     }
 	
 	// Update is called once per frame
 	void Update()
 	{
+        if (lightUpScript.flagLightUp == true) 
+        {
+            foreach (GameObject obj in enemy_list)
+            {
+                obj.GetComponent<Enemy>().enemy_state = Enemy.EnemyState.PATROL;
+            }
+        }
     }
 }
