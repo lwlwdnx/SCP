d5f7bde151ccffd9303848370d34d9960ab211a4
lambda1 <breakthrough.dodorara@gmail.com>
2019-09-22 12:22:11 +0900

    fix manager

--- Assets/Scripts/SCPManager.cs
+++ Assets/Scripts/SCPManager.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class SCPManager : MonoBehaviour
 {
     [SerializeField] GameObject[] enemys;
-    GameObject enemy_list;
+    List<GameObject> enemy_list = new List<GameObject>();
 
     // Use this for initialization
     void Start()
 	{
-		enemy_list = GameObject.Instantiate(enemys[0], transform.position, Quaternion.identity);
+		enemy_list.Add(GameObject.Instantiate(enemys[0], transform.position, Quaternion.identity));
 
-        enemy_list.GetComponent<Enemy>().position = new Vector3(10,0,0);
-        enemy_list.GetComponent<Enemy>().enemy_state = Enemy.EnemyState.CHASE;
+        foreach (GameObject obj in enemy_list)
+        {
+            obj.GetComponent<Enemy>().position = new Vector3(10, 0, 0);
+            obj.GetComponent<Enemy>().enemy_state = Enemy.EnemyState.CHASE;
+        }
     }
 	
 	// Update is called once per frame
