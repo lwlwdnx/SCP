50c98942c4a675f763e02bed0fba0026168f482a
lambda1 <breakthrough.dodorara@gmail.com>
2019-09-22 11:48:58 +0900

    add enemy

--- Assets/Scripts/SCPManager.cs
+++ Assets/Scripts/SCPManager.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class SCPManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] enemys;
+    GameObject enemy_list;
 
-
-
-	// Use this for initialization
-	void Start()
+    // Use this for initialization
+    void Start()
 	{
-		
-	}
+		enemy_list = GameObject.Instantiate(enemys[0], transform.position, Quaternion.identity);
+
+        enemy_list.GetComponent<Enemy>().position = new Vector3(10,0,0);
+        enemy_list.GetComponent<Enemy>().enemy_state = Enemy.EnemyState.CHASE;
+    }
 	
 	// Update is called once per frame
 	void Update()
