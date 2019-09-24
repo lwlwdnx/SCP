14be1a83a00f2fbbe4928d942052326c960da64d
Li Wanlong <lwlwdnx@gmail.com>
2019-09-24 12:14:07 +0900

    sound1

--- Assets/Scripts/BatteryManager.cs
+++ Assets/Scripts/BatteryManager.cs
@@ -28,15 +28,33 @@ public class BatteryManager : MonoBehaviour
     public GameObject battery10;
     public GameObject batteryLight10;
 
+    public GameObject battery11;
+    public GameObject batteryLight11;
+    public GameObject battery12;
+    public GameObject batteryLight12;
+    public GameObject battery13;
+    public GameObject batteryLight13;
+    public GameObject battery14;
+    public GameObject batteryLight14;
+
     public bool isLightUp = false;
     public GameObject nowLightUp;
     public Transform nowTransform;
 
+    public GameObject batterySound;
+
+    private bool batterySoundOn1 = false;
+    private bool batterySoundOn2 = false;
+
     // Use this for initialization
     void Start()
 	{
-		
-	}
+        //go = GameObject.FindWithTag("BatterySound");
+        if (batterySound != null)
+        {
+            batterySound.SetActive(false);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update()
@@ -52,30 +70,35 @@ public class BatteryManager : MonoBehaviour
             isLightUp = true;
             nowLightUp = battery01;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if(batteryLight02.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery02;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight03.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery03;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight04.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery04;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight05.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery05;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
 
         }
         else if (batteryLight06.GetComponent<Battery>().flagLightUp == true)
@@ -83,34 +106,70 @@ public class BatteryManager : MonoBehaviour
             isLightUp = true;
             nowLightUp = battery06;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight07.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery07;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight08.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery08;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight09.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery09;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else if (batteryLight10.GetComponent<Battery>().flagLightUp == true)
         {
             isLightUp = true;
             nowLightUp = battery10;
             nowTransform = nowLightUp.GetComponent<Transform>();
+            batterySoundOn1 = true;
         }
         else
         {
             isLightUp = false;
+            batterySoundOn1 = false;
+        }
+
+        if (batteryLight11.GetComponent<Battery>().flagLightUp == true)
+        {
+            batterySoundOn2 = true;
+        }
+        else if (batteryLight12.GetComponent<Battery>().flagLightUp == true)
+        {
+            batterySoundOn2 = true;
+        }
+        else if (batteryLight13.GetComponent<Battery>().flagLightUp == true)
+        {
+            batterySoundOn2 = true;
+        }
+        else if (batteryLight14.GetComponent<Battery>().flagLightUp == true)
+        {
+            batterySoundOn2 = true;
+        }
+        else
+        {
+            batterySoundOn2 = false;
+        }
+
+        if (batterySoundOn1 == true || batterySoundOn2 == true) 
+        {
+            batterySound.SetActive(true);
+        }
+        else
+        {
+            batterySound.SetActive(false);
         }
 
     }
